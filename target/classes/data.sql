4641e4fdefa68897c457469af67d5abf703f08bd
Robert Błach <robertbl1@wp.pl>
2020-09-21 22:33:55 +0200

    poprawki bazy

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -7,7 +7,7 @@ insert into category value (5, 'Produkty dla dorosłych', null);
 
 insert into author value (1, 'Agnieszka');
 insert into author value (2, 'Krystian');
-insert into author value (2, 'Robert');
+insert into author value (3, 'Robert');
 
 insert into product value (1, now(), 'Różowa chusta zrobiona z Ombre o długości 150m.', 'Różowa chusta', 199.99, 1, 2);
 insert into product value (2, now(), 'Niebiesko-zielona chusta zrobiona z Ombre o długości 100.', 'Morska chusta', 149.99, 1, 2);
